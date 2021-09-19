3f4408ccdbc94017fd3f24a8dcedf1ff2da3e71a
Paulo <paulo2782@gmail.com>
2021-09-18 22:06:43 -0300

    protocolo link

--- public/js/storage.js
+++ public/js/storage.js
@@ -1,7 +1,7 @@
 var localstorage = localStorage.getItem('special_link')
 var protocol     = 'https://';
 var iTam         = localstorage.length;
-
+var link         = protocol+localstorage.substr(7,iTam);
 $('#special_link').html(
-	"<a href='"+protocol+localstorage.substr(8,iTam)+"'>"+localstorage+"</a>"
+	"<a href='"+link+"'>"+link+"</a>"
 )
